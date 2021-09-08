a9c46ee201facf5bf221f9dcdcef67c9de5e7dba
Stefano Garrigó Ludvichak <stefanogl@gmail.com>
2021-09-08 14:08:14 -0300

    fix: removed unused code from app.tsx to avoid conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 import React from "react";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import RegisterForm from "./components/RegisterForm";
 function App() {
-  return (
-    <div className="App">
-      <RegisterForm />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
