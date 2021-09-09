71f9b54ba08699d50c6bb440c7308c77508935f9
Eduardo Pereira <augusto.pereira@protonmail.com>
2021-09-09 15:00:29 -0300

    fix: landing page import

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import GlobalStyle from "./styles/global";
-import { LandingPage } from "./pages/landingPage";
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <LandingPage />
     </div>
   );
 }
