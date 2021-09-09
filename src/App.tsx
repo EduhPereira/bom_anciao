3be30a42c1a57b08d8576f7c988578b64630c7ed
Eduardo Pereira <augusto.pereira@protonmail.com>
2021-09-09 14:53:53 -0300

    feat: landing page structure and initial style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import GlobalStyle from "./styles/global";
+import { LandingPage } from "./pages/landingPage";
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <LandingPage />
     </div>
   );
 }
---
+++ src/pages/landingPage/index.tsx
@@ -0,0 +1,54 @@
+import {
+  Header,
+  Functionalities,
+  Voluntaries,
+  InformationCard,
+  Institutions,
+  InstitutionsCard,
+  Footer,
+} from "./styles";
+export const LandingPage = () => {
+  return (
+    <>
+      <Header>
+        <span>bom ancião</span>
+        <div>
+          <button>Entrar</button>
+          <button>Cadastrar</button>
+        </div>
+      </Header>
+      <Functionalities>
+        <div className="func__col-1">
+          <img
+            src="http://recantoidosoaraxa-com-br.umbler.net/wp-content/uploads/2020/03/3-Desenho-Idosos.png"
+            alt=""
+          />
+        </div>
+        <div className="func__col-2">
+          <h1>Lorem Ipsum</h1>
+          <p>Lorem Ipsum</p>
+          <button>Começe Agora</button>
+        </div>
+      </Functionalities>
+      <Voluntaries>
+        <h2>Lorem Ipsum</h2>
+        <div>
+          <InformationCard>Information</InformationCard>
+          <InformationCard>Information</InformationCard>
+          <InformationCard>Information</InformationCard>
+        </div>
+      </Voluntaries>
+      <Institutions>
+        <h2>Lorem Ipsum</h2>
+        <div>
+          <InstitutionsCard>Information</InstitutionsCard>
+          <InstitutionsCard>Information</InstitutionsCard>
+          <InstitutionsCard>Information</InstitutionsCard>
+        </div>
+      </Institutions>
+      <Footer>
+        <h3>Footer</h3>
+      </Footer>
+    </>
+  );
+};
