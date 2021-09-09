c5e4774620b8b15ac0fc8b9318bff6e52eb84cb5
Stefano Garrigó Ludvichak <stefanogl@gmail.com>
2021-09-09 16:03:22 -0300

    institution search implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import InstitutionSearch from "./pages/InstitutionSearch";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <InstitutionSearch />
+    </div>
+  );
 }
 
 export default App;
