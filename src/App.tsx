ac6baba97126c3d653ad16b01b17eb31d09a0d1e
Mônica S <monica.s.bergues@gmail.com>
2021-09-12 17:23:59 -0300

    feat: date and time picker added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import LoginInstitution from "./pages/LoginInstitution";
 import Routes from "./routes";
 import GlobalStyle from "./styles/global";
+
 function App() {
   return (
     <div className="App">
       <Routes />
       <GlobalStyle />
-      <LoginInstitution />
     </div>
   );
 }
