427ac25abf2c7a3bb402fa45feafd357d638520e
Stefano Garrigó Ludvichak <stefanogl@gmail.com>
2021-09-15 16:43:31 -0300

    fix: user data post is now complete and push to login after post requisition

--- src/pages/UserRegister/index.tsx
+++ src/pages/UserRegister/index.tsx
@@ -6,27 +6,36 @@ import "react-toastify/dist/ReactToastify.css";
 import { Container, Content, Form, Input, Button, Aside } from "./styles";
 import api from "../../services/api";
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router";
 import oldman from "../../Assets/img/oldman.svg";
 import grandma from "../../Assets/img/grandma.svg";
-
+import { Redirect } from "react-router";
 interface IRegister {
-  user: string;
+  name: string;
   email: string;
-  cpf: number;
+  cpf: string;
+  city: string;
   address: string;
+  about: string;
+  type: string;
   password: string;
   confirmPassword: string;
 }
 
 const UserRegister = () => {
   const schema = yup.object().shape({
-    user: yup
+    name: yup
       .string()
       .required("Campo obrigatório")
       .matches(/^[aA-zZ\s]+$/, "Somente letras são permitidas"),
     email: yup.string().email("email inválido").required("Campo obrigatório"),
-
+    cpf: yup
+      .string()
+      .matches(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/, "CPF inválido.")
+      .required("Campo obrigatório"),
+    city: yup.string().required("Campo obrigatório"),
     address: yup.string().required("Campo obrigatório"),
+    about: yup.string().required("Campo obrigatório"),
     password: yup
       .string()
       .min(6, "Mínimo de oito caracteres")
@@ -35,7 +44,7 @@ const UserRegister = () => {
       .string()
       .oneOf([yup.ref("password")], "As senhas devem ser iguais"),
   });
-
+  const history = useHistory();
   const {
     register,
     handleSubmit,
@@ -43,12 +52,21 @@ const UserRegister = () => {
     formState: { errors },
   } = useForm<IRegister>({ resolver: yupResolver(schema) });
 
-  const handleRegister = ({ user, email, address, password }: IRegister) => {
+  const handleRegister = ({
+    name,
+    email,
+    cpf,
+    city,
+    address,
+    password,
+    type = "Voluntary",
+  }: IRegister) => {
     api
-      .post("/register", { user, email, address, password })
+      .post("/register", { name, email, cpf, city, address, password, type })
       .then(() => {
         reset();
         toast.success("Conta criada com sucesso!");
+        history.push("/login-voluntary");
       })
       .catch((err) => {
         if (err) {
@@ -66,23 +84,35 @@ const UserRegister = () => {
         <h2>Crie sua conta</h2>
         <Form onSubmit={handleSubmit(handleRegister)}>
           <label>Nome completo</label>
-          <Input type="text" {...register("user")} />
-          <span className="span-error">{errors.user?.message}</span>
+          <Input type="text" {...register("name")} />
+          <span className="span-error">{errors.name?.message}</span>
 
           <label>Email</label>
           <Input type="email" {...register("email")} />
           <span className="span-error">{errors.email?.message}</span>
 
+          <label>CPF:</label>
+          <Input type="text" {...register("cpf")} />
+          <span className="span-error">{errors.cpf?.message}</span>
+
+          <label>Cidade</label>
+          <Input type="text" {...register("city")} />
+          <span className="span-error">{errors.city?.message}</span>
+
           <label>Endereço</label>
           <Input type="text" {...register("address")} />
           <span className="span-error">{errors.address?.message}</span>
 
+          <label>Sobre você</label>
+          <Input type="text" {...register("about")} />
+          <span className="span-error">{errors.about?.message}</span>
+
           <label>Senha</label>
-          <Input type="text" {...register("password")} />
+          <Input type="password" {...register("password")} />
           <span className="span-error">{errors.password?.message}</span>
 
           <label>Confirme sua senha</label>
-          <Input type="text" {...register("confirmPassword")} />
+          <Input type="password" {...register("confirmPassword")} />
           <span className="span-error">{errors.confirmPassword?.message}</span>
           <Button type="submit">Enviar</Button>
           <div>
