5d202e412e2f1da6be6cba04e27da7e0d42f9bd1
Douglas Filipe <douglasfelipe.net77@gmail.com>
2021-09-16 12:02:37 -0400

    fix: Changing menu

--- src/components/voluntaryMenu/styles.ts
+++ src/components/voluntaryMenu/styles.ts
@@ -192,9 +192,9 @@ export const Logout = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    transform: translate(50%, 100%);
+    transform: translateX(50%);
     right: 50%;
-    bottom: 65%;
+    bottom: 5%;
     padding: 10px;
     color: #227475;
     font-weight: bold;
@@ -209,7 +209,7 @@ export const Logout = styled.div`
     }
 
     @media(min-width: 768px){
-        bottom: 30%;
+        bottom: 5%;
     }
 
 
